75ea0d0c812e64147835a4946052eaf4ba725652
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-11-21 08:16:54 -0300

    feat: added ms-menu into a eureka server

--- src/main/java/com/eats/msmenu/MsMenuApplication.java
+++ src/main/java/com/eats/msmenu/MsMenuApplication.java
@@ -2,8 +2,10 @@ package com.eats.msmenu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsMenuApplication {
 
 	public static void main(String[] args) {
